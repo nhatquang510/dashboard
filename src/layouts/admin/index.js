32b325bc20a12a6659c55c5f0bdb241a67777297
Hoang Tien <nguyenhoangtien21102002@gmail.com>
2023-12-17 02:12:52 +0700

    Change name

--- src/layouts/admin/index.js
+++ src/layouts/admin/index.js
@@ -126,7 +126,7 @@ export default function Dashboard(props) {
 							<Box>
 								<Navbar
 									onOpen={onOpen}
-									logoText={'Horizon UI Dashboard PRO'}
+									logoText={'Fire Dashboard PRO'}
 									brandText={getActiveRoute(routes)}
 									secondary={getActiveNavbar(routes)}
 									message={getActiveNavbarText(routes)}
